afe8cd58c2a966826a82fdb2a8d713d3c6937391
timss <timsateroy@gmail.com>
2014-03-09 22:31:12 +0100

    Start på konfigurasjonsfil, et vanlig python dict.

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Fyll inn feltene utifra valg på http://nav.no/minfastlege
+config = {
+    "office":        [], # legekontor
+    "doctors":       [], # liste av fastleger
+    "county":        "", # fylke
+    "municipality":  "", # kommune
+    "sex":           "", # m/k, kjønnet til legen
+    "accessibility": "", # ja/nei, tilgjengelig for funksjonshemmede m.fl.
+    "grouped":       "", # ja/nei, gruppepraksis på legesenter m/andre leger
+    "shared":        ""  # ja/nei, legen deler pasientliste m/andre leger
+}
