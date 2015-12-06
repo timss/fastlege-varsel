c7d513f30f357dbd107e2a46accd94582c078ee3
timss <timsateroy@gmail.com>
2015-12-06 20:15:58 +0100

    Parse table from soup:
    * For now the parsing is quite simple, but could easily
      do more such as mapping "Ja" or "Nei" to boolean,
      casting strings that are numbers to int or filtering
      columns that are not interesting.
    * Maybe most importantly, it's worth consdering adding
      county/communities to each document/dict, e.g. with
      a structure like so:
    
        {'county': {'id': cin, 'name': name}}
    
      Chose not to add it to parse_table as of now, due to
      unnecessary complexity and limited reusability.
      Maybe add it later by mapping over the list of dicts
      or something like that.

--- scrape.py
+++ scrape.py
@@ -10,6 +10,22 @@ def filter_tags(soup, tag, pat):
     tags = soup.find_all(tag, attrs={'value': re.compile(pat)})
     return {tag.attrs['value']: tag.text for tag in tags}
 
+def parse_table(soup):
+    """Return list of dicts matching table from soup"""
+    table = soup.find('table', attrs={'class': 'RTVtbl'})
+    thead = ['name', 'sex', 'office', 'accessibility', 'address',
+            'post', 'grouped', 'shared', 'citizens', 'available']
+
+    # list of dict {'available': 0', 'post': '4324 Sandnes', ...}
+    result = []
+
+    for row in table.find('tbody').find_all('tr'):
+        cols = [ele.text.strip() for ele in row.find_all('td')]
+        prac = dict(zip(thead, cols))
+        result.append(prac)
+
+    return result
+
 def scrape(url, county=None, community=None):
     """Scrape to soup, optionally limited to county and/or community"""
     payload = {"fylke": county, "kommune": community, "sok": "Søk etter fastlege"}
